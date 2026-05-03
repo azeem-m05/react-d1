a7e55af4008ad0620b6b79749d58903279d125cf
pgm.1 <113090461+Azeem-M99@users.noreply.github.com>
2026-05-03 13:30:33 +0530

    Added Product component and updated App, Math, and styles

--- basic-react-1/src/App.jsx
+++ basic-react-1/src/App.jsx
@@ -1,18 +1,20 @@
 import "./App.css";
 import Description from "./Description.jsx";
 import Math from "./Math.jsx";
+import ProductTab from "./ProductTab.jsx";
 import { Sum, Title } from "./Title.jsx";
 
 function App() {
   return (
-    <div>
+    <>
       <Title />
       <Description />
       <Sum />
       <Math />
       <h1>This is my app component</h1>
       <p>This is a paragraph</p>
-    </div>
+      <ProductTab />
+    </>
   );
 }
 
--- basic-react-1/src/Math.jsx
+++ basic-react-1/src/Math.jsx
@@ -1,4 +1,12 @@
 function Math() {
-  return <>2 * 2 = {2 * 2}</>;
+  let name = "habibi azeem";
+  return (
+    <>
+      2 * 2 = {2 * 2}
+      <h3>Hi, {name.toUpperCase()}</h3>
+    </>
+  );
 }
 export default Math;
+
+// whats ever u write inside this->{}, it will act as pure JavaScript
