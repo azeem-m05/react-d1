a668351706aab2a4dfc95baebbf96df05eb7b353
pgm.1 <113090461+Azeem-M99@users.noreply.github.com>
2026-05-06 07:19:18 +0530

    Fixed product component

--- basic-react-1/src/Product.jsx
+++ basic-react-1/src/Product.jsx
@@ -1,10 +1,12 @@
 import "./Product.css";
 
-function Product() {
+function Product({ title, Price = 1, features, features2 }) {
   return (
     <div className="Product">
-      <h3>Product title</h3>
-      <h5>Product Description</h5>
+      <h2>{title}</h2>
+      <h4>Price : {Price}</h4>
+      <h5>{features}</h5>
+      <h6>{features2}</h6>
     </div>
   );
 }
--- basic-react-1/src/ProductTab.jsx
+++ basic-react-1/src/ProductTab.jsx
@@ -1,13 +1,30 @@
 import Product from "./Product.jsx";
 
 function ProductTab() {
+  let options = ["hi-tech", "durable", "compatible"];
+  let options2 = ["hi-tech", "durable", "compatible"];
+  let options3 = ["hi-tech", "durable", "compatible"];
+
   return (
     <>
-      <Product />
-      <Product />
-      <Product />
+      <Product
+        title="Phone"
+        Price={30000}
+        features={options}
+        features2={options2}
+      />
+      <Product
+        title="Laptop"
+        Price={50000}
+        features={options}
+        features={options2}
+      />
+      <Product title="Watch" features={options} features={options3} />
     </>
   );
 }
 
 export default ProductTab;
+
+// u need to apply curly braces to show numbers as numbers
+// if u haven't give price to a product then u can set default price from the props
